8197028bbc4c97f2cdc756b1220514af6f37e80b
Jose Manuel <soldadojimenezj@gmail.com>
2026-06-25 18:15:28 +0200

    feat: add UserRepository and UserService

--- src/main/java/com/oceandive/backend/user/UserRepository.java
+++ src/main/java/com/oceandive/backend/user/UserRepository.java
@@ -1,5 +1,12 @@
 package com.oceandive.backend.user;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository extends JpaRepository <User,  UUID> {
+    //Busca un usuario por su ID
+    Optional<User> findByid(UUID id);
+    //Busca a todos los usuarios por 
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/oceandive/backend/user/UserService.java
+++ src/main/java/com/oceandive/backend/user/UserService.java
@@ -1,5 +1,19 @@
 package com.oceandive.backend.user;
 
-public class UserService {
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public Optional<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+    public Optional<User> findByid(UUID id){
+        return userRepository.findByid(id);
+    }
 }
